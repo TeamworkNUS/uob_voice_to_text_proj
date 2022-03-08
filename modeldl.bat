aeaf3ad96e1e803a9cbba98e6a937a70e4cc2dbb
ZirconiumZr <zengrui1994@hotmail.com>
2022-03-08 18:18:11 +0800

    update requirements.txt

--- modeldl.bat
+++ modeldl.bat
@@ -2,11 +2,11 @@
 echo model download start...
 echo ###################################################################################
 wget https://alphacephei.com/kaldi/models/vosk-model-en-us-0.22.zip --no-check-certificate
-###################################################################################
+echo ###################################################################################
 echo model download success!
 echo ###################################################################################
 echo unzip model...
-jar xf vosk-model-en-us-0.22.zip
+tar -xf vosk-model-en-us-0.22.zip
 echo ###################################################################################
 echo move model to folder...
 MOVE vosk-model-en-us-0.22 model
