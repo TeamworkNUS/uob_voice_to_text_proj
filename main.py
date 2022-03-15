38757726b44c9ffbbf0998bdb73fc5dcba6dbebe
ZirconiumZr <zengrui1994@hotmail.com>
2022-03-15 16:09:31 +0800

    update main.py
    merge ffmpeg func to main.py

--- main.py
+++ main.py
@@ -212,7 +212,7 @@ print('duration: ', endtime-starttime)
 
 final_sd_result = pd.DataFrame(tem_sd_result, columns=['index','starttime','endtime','duration','speaker_label'])
 print(final_sd_result)
-quit()  # TODO: comment after STT section is done completely
+# quit()  # TODO: comment after STT section is done completely
 
 ###  Cut audio by SD result
 namef, namec = os.path.splitext(AUDIO_NAME)
@@ -224,20 +224,55 @@ uob_mainprocess.cut_audio_by_timestamps(start_end_list=tem_sd_result, audioname=
 print('*'*30, 'Cut Slices Done')
 
 ###  Speech to Text Conversion
-output=[]
+# Load VOSK model
+from vosk import Model, KaldiRecognizer, SetLogLevel
+print('*'*30, 'Loading Model')
+sample_rate=16000
+model = Model("model")
+rec = KaldiRecognizer(model, sample_rate)
+
+# STT Start
+import wave
+import json
+
+print('*'*30, 'STT Start')
+transcription = []
+
 stt = pd.DataFrame(columns=['index', 'text'])
-for filename in os.listdir("./stt/"): #(slices_path+"/"):
+for filename in os.listdir("./audio/"): #(slices_path+"/"):
     if filename.endswith(".wav"): 
         namef, namec = os.path.splitext(filename)
         namef_other, namef_index = namef.rsplit("_", 1)
         namef_index = int(namef_index)
         
+        wf = wave.open("./audio/"+filename, "rb")   # Change when confirm
         # file = slices_path+"/"+filename
-        
-        text = subprocess.check_output(["python","ffmpeg.py",filename],cwd= './stt').decode("utf-8")
-        textNew = re.findall('"([^"]*)"', text)
-        index = namef_index  # index = re.findall(r'\d+',filename)[-1]
-        stt.loc[len(stt)] = [index, textNew[1]]
+        while True:
+            data = wf.readframes(10000000)
+            if len(data) == 0:
+                break
+            if rec.AcceptWaveform(data):
+                # Convert json output to dict
+                result_dict = json.loads(rec.Result())
+                # Extract text values and append them to transcription list
+                transcription.append(result_dict.get("text", ""))
+
+        # Get final bits of audio and flush the pipeline
+        final_result = json.loads(rec.FinalResult())
+        transcription.append(final_result.get("text", ""))
+
+        # merge or join all list elements to one big string
+        transcription_text = ' '.join(transcription)
+        print(transcription_text)  # For output check only, remove after confirm.
+
+        stt.loc[len(stt)] = [namef_index, transcription_text]
+        transcription_text = ""
+        transcription = []
+
+        # text = subprocess.check_output(["python","ffmpeg.py",filename],cwd= './stt').decode("utf-8")
+        # textNew = re.findall('"([^"]*)"', text)
+        # index = namef_index  # index = re.findall(r'\d+',filename)[-1]
+        # stt.loc[len(stt)] = [index, textNew[1]]
         
 # stt['index'] = stt['index'].astype(int)
 print(stt)
